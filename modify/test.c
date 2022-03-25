ffeb6d1d06d7cc981e68ecad8eb5f40e3d44cfe4
Letheward <letheward@foxmail.com>
2022-03-25 10:56:17 +0800

    add comments

--- modify/test.c
+++ modify/test.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// this will be modified by modify.c
+// useful for pre-fill large amount of global data, instead of print the data to text values
+// if only we have compile-time execution...
 unsigned char data[1024 * 1024] = {
     'm', 'a', 'g', 'i', 'c', 's', 't', 'a', 'm', 'p',
 };
