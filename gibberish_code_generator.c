e23eefbcefa606af57dba481b484f8b013278b6e
Letheward <letheward@foxmail.com>
2022-06-11 16:55:18 +0800

    remove unused stuff, catalog

--- gibberish_code_generator.c
+++ gibberish_code_generator.c
@@ -26,12 +26,6 @@ typedef struct {
     u64 count;
 } String;
 
-typedef struct {
-    u8* data;
-    u64 count;
-    u64 allocated;
-} StringBuilder;
-
 
 
 
@@ -112,56 +106,15 @@ void temp_info() {
 
 
 
-/* ==== Utils ==== */
-
-void error(char* s, ...) {
-
-    va_list va;
-    va_start(va, s);
-
-    printf("[Error] ");
-    vprintf(s, va);
-
-    exit(1); 
-}
-
-void generate_random_seed() {
-    struct timespec start;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    srand(start.tv_nsec);
-}
-
-
-
-
-
-/* ==== Timer ==== */
-
-typedef struct {
-    struct timespec start;
-    struct timespec end;
-    u8 which;
-} PerformanceTimer;
-
-PerformanceTimer timer;
-
-void time_it() {
-    if (timer.which == 0) {
-        clock_gettime(CLOCK_MONOTONIC, &timer.start);
-        timer.which = 1;
-    } else {
-        clock_gettime(CLOCK_MONOTONIC, &timer.end);
-        printf("%fs\n", (timer.end.tv_sec - timer.start.tv_sec) + 1e-9 * (timer.end.tv_nsec - timer.start.tv_nsec));
-        timer.which = 0;
-    }
-}
-
-
 
 
 
 
 /* ==== String: Basic ==== */
+
+void upper_case_first_char(String in) {
+    in.data[0] += 'A' - 'a';
+}
 
 String string_advance(String s, u64 p) {
     return (String) {s.data + p, s.count - p};
@@ -200,41 +153,7 @@ String string_find(String a, String b) {
     return (String) {0};
 }
 
-String string_concat(u64 count, ...) {
-    
-    u64 size = sizeof(String) * count;
-    Array(String) strings = {
-        .data  = temp_alloc(size),
-        .count = count,
-    };
-
-    String out = {0};
-    {
-        va_list args;
-        va_start(args, count);
-        for (int i = 0; i < count; i++) {
-            String s = va_arg(args, String);
-            strings.data[i] = s;
-            out.count += s.count;
-        }
-        va_end(args);
-    }
-
-    out.data = runtime.alloc(out.count);
-    
-    u64 counter = 0;
-    for (int i = 0; i < count; i++) {
-        String s = strings.data[i];
-        for (int j = 0; j < s.count; j++) {
-            out.data[counter + j] = s.data[j];
-        }
-        counter += s.count;
-    }
-    
-    return out;
-}
-
-String string_concat_array(Array(String) strings) {
+String string_concat(Array(String) strings) {
     
     u64 count = 0;
     for (u64 i = 0; i < strings.count; i++) {
@@ -318,220 +237,42 @@ String string_replace(String s, String a, String b) {
 
 
 
-/* ==== String: Formatting ==== */
+/* ==== Print ==== */
 
-String format_s32(s32 value, s32 base) {
-
-    u8* table = (u8*) "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@_";
-
-    if (value == 0) return string("0"); // quick fix, is this fast?
-    if (base < 2 || base > 64) return (String) {0};
-
-    u8 digits[32] = {0}; // todo: speed? just write it to the malloc() buffer?
-    s32 sign = value >> 31;
-    s32 count;
-    {
-        s32 temp = sign ? -value : value;
-        for (count = 0; temp > 0; count++) {
-            digits[count] = table[temp % base]; // what about custom digits?
-            temp /= base;
-        }
-    }
-
-    u8* data = temp_alloc(32);
-    if (sign) {data[0] = '-'; count++;}
-    for (int i = sign ? 1 : 0; i < count; i++) data[i] = digits[count - i - 1];
-
-    return (String) {data, count};
+// basic print
+void print_string(String s) {
+    for (int i = 0; i < s.count; i++) putchar(s.data[i]);
 }
 
-// todo: accuracy issue, handle infinity and NaN
-String format_f32(float value) {
-
-    u8* table = (u8*) "0123456789";
-    
-    u32 data; 
-    {
-        u32 * p = (u32*) &value; // workaround for aliasing warning
-        data = *p; 
-    }
-
-    // get data from IEEE 754 bits
-    int sign = (data >> 31);
-    int exp  = (data << 1 >> 24) - 127 - 23;
-    int frac = (data << 9 >> 9) | 0x800000; // add the implicit "1." in ".1010010"
-
-    u64 temp = frac * 1000000000000; // todo: accuracy 
-
-    if (exp < 0) for (int i = 0; i > exp; i--) temp /= 2;
-    else         for (int i = 0; i < exp; i++) temp *= 2;
-
-    u8 digits[64] = {0};
-    int count; // digit count
-    for (count = 0; temp > 0; count++) {
-        digits[count] = table[temp % 10]; 
-        temp /= 10;
-    }
-    
-    u8* result = temp_alloc(64);
-    u64 result_count = 0;
-    
-    if (sign) result[0] = '-';
-    int dot_pos = count - 12; // todo: find out why it's 12
-    
-    if (dot_pos <= 0) {
-        result_count = count - dot_pos + 2 + sign;
-        result[sign    ] = '0'; 
-        result[sign + 1] = '.';
-        for (int i = 0; i < -dot_pos; i++) result[sign + 2 + i] = '0'; 
-        for (int i = 0; i < count; i++) {
-            result[sign - dot_pos + i + 2] = digits[count - i - 1]; 
-        }
-    } else {
-        result_count = count + sign + 1;
-        result[sign + dot_pos] = '.';
-        for (int i =       0; i < dot_pos; i++) result[sign + i    ] = digits[count - i - 1];
-        for (int i = dot_pos; i <   count; i++) result[sign + i + 1] = digits[count - i - 1];
-    }
-
-    return (String) {result, result_count};
-}
-
-// modified from a [public domain library](https://github.com/badzong/base64/)
-String base64_encode(String in) {
-    
-    const u8* table = (u8*) "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-    // allocate buffer
-    u64 count = 4 * ((in.count + 2) / 3);
-    u8* data  = runtime.alloc(count);
-
-    // convert
-    u64 j = 0;
-    for (u64 i = 0; i < in.count; i += 3) {
-        
-        u32 a = in.data[i    ];
-        u32 b = in.data[i + 1];
-        u32 c = in.data[i + 2];
-
-        u32 triple = (a << 16) + (b << 8) + c;
-
-        data[j    ] = table[(triple >> 18) & 0x3f];
-        data[j + 1] = table[(triple >> 12) & 0x3f];
-        data[j + 2] = table[(triple >>  6) & 0x3f];
-        data[j + 3] = table[(triple      ) & 0x3f];
-
-        j += 4;
-    }
-
-    // padding at the end
-    switch (in.count % 3) {
-        case 1: data[j - 2] = '=';
-        case 2: data[j - 1] = '=';
-    }
-
-    return (String) {data, count};
-}
-
-// todo: not robust, no escape with @, need more testing
+// todo: not robust, no escape with @, need more testing, deal with switching back allocator
 void print(String s, ...) {
-
-    Array(String) chunks = string_split(s, string("@"));
     
-    if (chunks.count < 2) {
-        for (int i = 0; i < s.count; i++) putchar(s.data[i]);
-        return;
-    }
+    void* (*old_alloc)(u64) = runtime.alloc; // ehh....
+    runtime.alloc = temp_alloc;
+    
+    Array(String) chunks = string_split(s, string("@"));
+   
+    runtime.alloc = old_alloc;
+    
+    if (chunks.count < 2) { print_string(s); return; }
     
     u64 arg_count = chunks.count - 1;
-    String* args_data = temp_alloc(sizeof(String) * arg_count);
+    va_list args;
+    va_start(args, s);
 
-    {
-        va_list args;
-        va_start(args, s);
-        for (u64 i = 0; i < arg_count; i++) args_data[i] = va_arg(args, String);
-        va_end(args);
-    }
-
-    String c = chunks.data[0];
-    for (u64 j = 0; j < c.count; j++) putchar(c.data[j]);
-
+    print_string(chunks.data[0]);
     for (u64 i = 0; i < arg_count; i++) {
-        String c   = chunks.data[i + 1];
-        String arg = args_data[i]; 
-        for (u64 j = 0; j < arg.count; j++) putchar(arg.data[j]);
-        for (u64 j = 0; j < c.count;   j++) putchar(c.data[j]);
+        print_string(va_arg(args, String)); // not safe, but this is C varargs, what can you do 
+        print_string(chunks.data[i + 1]);
     }
 
-    temp_free(sizeof(String) * arg_count);
+    va_end(args);
 }
 
 
 
 
 
-
-/* ==== File IO ==== */
-
-String load_file(char* path) {
-
-    FILE* f = fopen(path, "rb");
-    if (!f) error("Cannot load %s\n", path); 
-
-    u8* data;
-    u64 count;
-
-    fseek(f, 0, SEEK_END);
-    count = ftell(f);
-    fseek(f, 0, SEEK_SET);
-
-    data = malloc(count);
-    fread(data, 1, count, f);
-    fclose(f);
-
-    return (String) {data, count};
-}
-
-void save_file(String in, char* path) {
-
-    FILE* f = fopen(path, "wb");
-    if (!f) error("Cannot open file %s\n", path); 
-
-    fwrite(in.data, sizeof(u8), in.count, f);
-    fflush(f);
-    fclose(f);
-}
-
-
-
-
-/* ==== C Interface ==== */
-
-void program();
-
-int main(int arg_count, char** args) {
-
-    // setup runtime
-    u64 size = 1024 * 256;
-    runtime.temp_buffer.data = calloc(size, sizeof(u8));
-    runtime.temp_buffer.size = size;
-    runtime.alloc = malloc;
-    
-    runtime.command_line_args = (Array(String)) {
-        .data  = malloc(sizeof(String) * arg_count),
-        .count = arg_count,
-    };
-
-    for (int i = 0; i < arg_count; i++) {
-        int len = strlen(args[i]);
-        String* s = &runtime.command_line_args.data[i];
-        s->data  = malloc(len);       
-        s->count = len;
-        memcpy(s->data, args[i], len);
-    }
-
-    program();
-}
 
 
 
@@ -543,7 +284,55 @@ int main(int arg_count, char** args) {
 String alphabet[4];
 String atomic_types[11];
 
+char* bullshit_catalog[] = {
+    "object",
+    "class",
+    "interface",
+    "builder",
+    "string",
+    "friend",
+    "factory",
+    "tester",
+    "counter",
+    "converter",
+    "observer",
+    "initializer",
+    "coverage",
+    "pointer",
+    "hash",
+    "queue",
+    "stack",
+    "list",
+    "vector",
+    "pair",
+    "tuple",
+    "align",
+    "concept",
+    "decl",
+    "dynamic",
+    "explicit",
+    "exporter",
+    "mutable",
+    "namespace",
+    "private",
+    "protected",
+    "public",
+    "synchronized",
+    "template",
+    "virtual",
+    "ordered",
+    "unordered",
+    "regex",
+    "mutex",
+    "lock",
+    "unlock",
+};
 
+// leak, use temp_alloc() for all for now
+String get_random_word_from_bullshit_catalog() {
+    char* s = bullshit_catalog[rand() % length_of(bullshit_catalog)];
+    return string_copy((String) {(u8*) s, strlen(s)});
+}
 
 String get_random_word(int length) {
     
@@ -582,10 +371,6 @@ String get_random_word(int length) {
     return out;
 }
 
-void upper_case_first_char(String in) {
-    in.data[0] += 'A' - 'a';
-}
-
 
 typedef enum {
     STYLE_SNAKE,         // snake_case               
@@ -594,13 +379,14 @@ typedef enum {
     STYLE_PASCAL_SNAKE,  // Pascal_Snake_Case               
 } CodeStyle;
 
+// leak, use temp_alloc() for all for now
 String get_random_identifier(int count, int word_min, int word_max, CodeStyle style) {
     
     u8 more = (style == STYLE_SNAKE || style == STYLE_PASCAL_SNAKE) && count > 1;
     if (more) count += count - 1;
 
     Array(String) ss = {temp_alloc(sizeof(String) * count), count};
-    int rand_length = word_max - word_min;
+    //int rand_length = word_max - word_min;
 
     for (int i = 0; i < count; i++) {
 
@@ -609,7 +395,8 @@ String get_random_identifier(int count, int word_min, int word_max, CodeStyle st
             continue;
         }
 
-        ss.data[i] = get_random_word(rand() % rand_length + word_min);
+        ss.data[i] = get_random_word_from_bullshit_catalog();
+        //ss.data[i] = get_random_word(rand() % rand_length + word_min);
 
         if (style != STYLE_SNAKE) {
             if (style == STYLE_CAMEL && i == 0) continue;
@@ -617,7 +404,7 @@ String get_random_identifier(int count, int word_min, int word_max, CodeStyle st
         }
     }
     
-    return string_concat_array(ss);
+    return string_concat(ss);
 }
 
 
@@ -641,7 +428,7 @@ void print_type_to_code(TypeInfo type) {
     print(string("} @;\n\n"), type.name);
 }
 
-
+// leak, use temp_alloc() for all for now
 TypeInfo get_random_struct_using_name_pool(u64 field_count, Array(String) pool) {
     
     TypeInfo out;
@@ -655,7 +442,7 @@ TypeInfo get_random_struct_using_name_pool(u64 field_count, Array(String) pool) 
     
     for (u64 i = 0; i < count; i++) {
         out.fields.data[i * 2    ] = pool.data[rand() % pool.count];
-        out.fields.data[i * 2 + 1] = get_random_identifier(rand() % 3 + 1, 3, 7, STYLE_SNAKE);
+        out.fields.data[i * 2 + 1] = get_random_identifier(rand() % 3 + 1, 3, 7, STYLE_CAMEL);
     }
 
     return out;
@@ -664,12 +451,22 @@ TypeInfo get_random_struct_using_name_pool(u64 field_count, Array(String) pool) 
 
 
 
+int main() {
 
-
-
-
-void program() {
-
+    // setup runtime
+    {
+        u64 size = 1024 * 256;
+        runtime.temp_buffer.data = calloc(size, sizeof(u8));
+        runtime.temp_buffer.size = size;
+        runtime.alloc = malloc;
+    }
+    
+    // generate random seed
+    {
+        struct timespec start;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        srand(start.tv_nsec);
+    }
 
     // gcc won't let us use literals in -pedantic
     {
@@ -691,48 +488,41 @@ void program() {
         atomic_types[10] = string("String");
     }
 
-    generate_random_seed();
+    printf(
+        "typedef unsigned char           u8;\n"
+        "typedef unsigned short int      u16;\n"
+        "typedef unsigned int            u32;\n"
+        "typedef unsigned long long int  u64;\n"
+        "typedef signed char             s8;\n"
+        "typedef signed short            s16;\n"
+        "typedef signed int              s32;\n"
+        "typedef signed long long int    s64;\n"
+        "typedef float                   f32;\n"
+        "typedef double                  f64;\n"
+        "\n"
+        "typedef struct {\n"
+        "    u8* data;\n"
+        "    u64 count;\n"
+        "} String;\n\n"
+    );
     
     u64 count = 32;
     Array(String) type_name_pool = {malloc(sizeof(String) * count), count};
     TypeInfo*     types          =  malloc(sizeof(TypeInfo) * count);
 
-    print(
-        string(
-            "typedef unsigned char           u8;\n"
-            "typedef unsigned short int      u16;\n"
-            "typedef unsigned int            u32;\n"
-            "typedef unsigned long long int  u64;\n"
-            "typedef signed char             s8;\n"
-            "typedef signed short            s16;\n"
-            "typedef signed int              s32;\n"
-            "typedef signed long long int    s64;\n"
-            "typedef float                   f32;\n"
-            "typedef double                  f64;\n"
-            "\n"
-            "typedef struct {\n"
-            "    u8* data;\n"
-            "    u64 count;\n"
-            "} String;\n\n"
-        )
-    );
-
+    runtime.alloc = temp_alloc;
+    
     for (u64 i = 0; i < count; i++) {
         types[i] = get_random_struct_using_name_pool(rand() % 5 + 2, array(String, atomic_types));
         type_name_pool.data[i] = types[i].name;
     }
     
-    for (u64 i = 0; i < count; i++) {
-        print_type_to_code(types[i]);
-    }
-    
-    temp_reset();
-
-    runtime.alloc = temp_alloc;
+    for (u64 i = 0; i < count; i++) print_type_to_code(types[i]);
     
     for (u64 i = 0; i < 64; i++) {
         print_type_to_code(get_random_struct_using_name_pool(rand() % 5 + 2, type_name_pool));
     }
+    
     temp_reset();    
 
     printf(
@@ -742,3 +532,4 @@ void program() {
         "}\n"
     );
 }
+
